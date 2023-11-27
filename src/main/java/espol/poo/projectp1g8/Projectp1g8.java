d6634935e2b795f3d35ff6d6d412728def69a42b
William <William@172.19.185.86>
2023-11-27 12:52:32 -0500

    Creacion de clase TipoSectores

--- src/main/java/espol/poo/projectp1g8/Projectp1g8.java
+++ src/main/java/espol/poo/projectp1g8/Projectp1g8.java
@@ -1,13 +1,7 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- */
-
 package espol.poo.projectp1g8;
 
-/**
- *
- * @author William
- */
+import model.*;
+
 public class Projectp1g8 {
 
     public static void main(String[] args) {
---
+++ src/main/java/model/TipoSectores.java
@@ -0,0 +1,8 @@
+package model;
+
+public enum TipoSectores {
+    ALIMENTACION,
+    EDUCACION,
+    SALUD,
+    VESTIMENTA
+}
