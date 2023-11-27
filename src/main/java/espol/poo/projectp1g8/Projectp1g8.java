75f6b23e014c62dde1160c1487913e5cfa3cb866
William Mayorga <73685388+Kiliam84@users.noreply.github.com>
2023-11-27 11:32:24 -0500

    Adición de la clase principal
    Adición de la clase principal con su respectivo directorio.

---
+++ src/main/java/espol/poo/projectp1g8/Projectp1g8.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package espol.poo.projectp1g8;
+
+/**
+ *
+ * @author William
+ */
+public class Projectp1g8 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
