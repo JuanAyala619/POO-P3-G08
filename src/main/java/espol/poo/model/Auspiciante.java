0a2be8841acbcf7643097d27ad8cb74892476975
Cristhian060303 <Cris@DESKTOP-TRPM1ES>
2023-11-28 23:05:38 -0500

    Correccion clase Auspiciante

--- src/main/java/espol/poo/model/Auspiciante.java
+++ src/main/java/espol/poo/model/Auspiciante.java
@@ -1,10 +1,12 @@
 package espol.poo.model;
 import java.util.ArrayList;
+import java.util.Scanner;
 
 
 public class Auspiciante extends Persona{
   private ArrayList<TipoSectores> lstTipoSectores;
 
+  //Constructor de un objeto Auspiciante
   public Auspiciante(String ruc, String nombre, String telefono, String email, String direccion, String sitioWeb, String personaResponsable){
     super(ruc, nombre, telefono, email, direccion, sitioWeb, personaResponsable);
     this.lstTipoSectores = new ArrayList<TipoSectores>();
@@ -18,8 +20,17 @@ public class Auspiciante extends Persona{
     return this.lstTipoSectores;
   }
   
-  public void agregarSectores(){
-      a
+  public void agregarSectores(int opcion){
+    if(opcion == 1){
+        this.lstTipoSectores.add(TipoSectores.ALIMENTACION);
+    }else if(opcion == 2){
+        this.lstTipoSectores.add(TipoSectores.EDUCACION);
+    }else if(opcion == 3){
+        this.lstTipoSectores.add(TipoSectores.SALUD);
+    }else if(opcion == 4){
+        this.lstTipoSectores.add(TipoSectores.VESTIMENTA);
+    }
+    
   }
   
 }
