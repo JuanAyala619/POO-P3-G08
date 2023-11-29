277eb0be0eb4f58b5c4b152f7180695c9cffa9ec
Kiliam84 <github.com/Kiliam84>
2023-11-29 17:59:17 -0500

    Implementacion del menu de administracion de emprendedores completa

--- src/main/java/espol/poo/projectp1g8/MenuEmprendedores.java
+++ src/main/java/espol/poo/projectp1g8/MenuEmprendedores.java
@@ -19,7 +19,7 @@ public class MenuEmprendedores {
                 System.out.println("Mail: " + e.getEmail());
                 System.out.println();
             }
-            System.out.println("Menu de opciones");
+            System.out.println("Menu de administracion de emprendedores");
             System.out.println("""
                                1. Registrar emprendedor
                                2. Editar emprendedor
@@ -88,13 +88,123 @@ public class MenuEmprendedores {
                 }
                 case '2' -> {
                     //Codigo para editar emprendedor
-                    
+                    String ruc;
+                    String opcionEdit;
+                    System.out.println("Editar emprendedor");
+                    do{
+                    System.out.println("Ingrese el RUC o cedula del emprendedor");
+                    ruc = sc.nextLine();
+                    if(Projectp1g8.buscarPersona(ruc) == null || !(Projectp1g8.buscarPersona(ruc) instanceof Emprendedor)){
+                        System.out.println("El RUC o cedula ingresado no pertenese a ningun emprendedor registrado");
+                    }
+                    }while(Projectp1g8.buscarPersona(ruc) == null || !(Projectp1g8.buscarPersona(ruc) instanceof Emprendedor));
+                    System.out.println("Informacion actual de " + Projectp1g8.buscarPersona(ruc).getNombre());
+                    System.out.println(Projectp1g8.buscarPersona(ruc));
+                    System.out.println("¿Que campo desea editar?");
+                    System.out.println("1. Nombre");
+                    System.out.println("2. Telefono");
+                    System.out.println("3. Email");
+                    System.out.println("4. Direccion");
+                    System.out.println("5. Sitio Web");
+                    System.out.println("6. Persona Responsable");
+                    System.out.println("7. Redes Sociales");
+                    System.out.println("8. Descripcion de servicios");
+                    do{
+                        System.out.print("Ingrese numero de opcion: ");
+                        opcionEdit = sc.nextLine() + " ";
+                        switch(opcionEdit.charAt(0)){
+                            case '1' -> {
+                                System.out.println("Ingrese el nuevo nombre");
+                                String nuevoNombre = sc.nextLine();
+                                Projectp1g8.buscarPersona(ruc).setNombre(nuevoNombre);
+                                System.out.println("Nombre modificado con exito.");
+                            }
+                            case '2' -> {
+                                System.out.println("Ingrese el nuevo telefono");
+                                String nuevoTelefono = sc.nextLine();
+                                Projectp1g8.buscarPersona(ruc).setTelefono(nuevoTelefono);
+                                System.out.println("Telefono modificado con exito.");
+                            }
+                            case '3' -> {
+                                System.out.println("Ingrese el nuevo Email");
+                                String nuevoEmail = sc.nextLine();
+                                Projectp1g8.buscarPersona(ruc).setEmail(nuevoEmail);
+                                System.out.println("Email modificado con exito.");
+                            }
+                            case '4' -> {
+                                System.out.println("Ingrese la nueva direccion");
+                                String nuevaDir = sc.nextLine();
+                                Projectp1g8.buscarPersona(ruc).setDireccion(nuevaDir);
+                                System.out.println("Direccion modificada con exito.");
+                            }
+                            case '5' -> {
+                                System.out.println("Ingrese el nuevo sitio web");
+                                String nuevoSitio = sc.nextLine();
+                                Projectp1g8.buscarPersona(ruc).setTelefono(nuevoSitio);
+                                System.out.println("Sitio web modificado con exito.");
+                            }
+                            case '6' -> {
+                                System.out.println("Ingrese el nombre de la nueva persona responsable");
+                                String nuevoResponsable = sc.nextLine();
+                                Projectp1g8.buscarPersona(ruc).setPersonaResponsable(nuevoResponsable);
+                                System.out.println("Nombre de persona responsable modificado con exito.");
+                            }
+                            case '7' -> {
+                                if(Projectp1g8.buscarPersona(ruc).getRedesSociales().isEmpty()){
+                                    System.out.println("\nEste emprendedor no tiene niguna red social\n");
+                                }else{
+                                    System.out.println("Lista de redes sociales del emprendedor");
+                                    System.out.println(Projectp1g8.buscarPersona(ruc).getRedesSociales());
+                                    int ind;
+                                    do{
+                                        System.out.println("Escriba el nombre de la red social que de desea editar");
+                                        String tipo = sc.nextLine();
+                                        TipoRedSocial red = TipoRedSocial.Twitter;
+                                        if (tipo.equals("Twitter")) {
+                                            red = TipoRedSocial.Twitter;
+                                        } else if (tipo.equals("Facebook")) {
+                                            red = TipoRedSocial.Facebook;
+                                        } else if (tipo.equals("Instagram")) {
+                                            red = TipoRedSocial.Instagram;
+                                        } else if (tipo.equals("Youtube")) {
+                                            red = TipoRedSocial.Youtube;
+                                        } else if (tipo.equals("TikTok")) {
+                                            red = TipoRedSocial.TikTok;
+                                        } else if (tipo.equals("LinkedIn")) {
+                                            red = TipoRedSocial.LinkedIn;
+                                        } else if (tipo.equals("Pinterest")) {
+                                            red = TipoRedSocial.Pinterest;
+                                        } else {}
+                                        ind = Projectp1g8.buscarPersona(ruc).getRedesSociales().indexOf(new RedSocial(red,"",""));
+                                        if(ind == -1){
+                                            System.out.println("Por favor escriba un nombre valido");
+                                        }
+                                    }while(ind == -1);
+                                    System.out.println("Escriba el nuevo nombre de usuario");
+                                    String nuevoNombre = sc.nextLine();
+                                    System.out.println("Escriba el nuevo link del usuario");
+                                    String nuevoLink = sc.nextLine();
+                                    Projectp1g8.buscarPersona(ruc).getRedesSociales().get(ind).setUsuario(nuevoNombre);
+                                    Projectp1g8.buscarPersona(ruc).getRedesSociales().get(ind).setEnlace(nuevoLink);
+                                    System.out.println("Red social modificada con exito");
+                                }
+                            }
+                            case '8' -> {
+                                Emprendedor e = (Emprendedor)Projectp1g8.buscarPersona(ruc);
+                                System.out.println("Ingrese la nueva descripcion");
+                                String nuevaDesc = sc.nextLine();
+                                e.setDescripcionServicios(nuevaDesc);
+                                System.out.println("Descripcion de servicios modificado con exito.");
+                            }
+                            default -> System.out.println("Opción no válida. Por favor, ingresa un número del 1 al 8.");
+                        }
+                    }while(opcionEdit.charAt(0) != '1' && opcionEdit.charAt(0) != '2' && opcionEdit.charAt(0) != '3' && opcionEdit.charAt(0) != '4' && opcionEdit.charAt(0) != '5' && opcionEdit.charAt(0) != '6' && opcionEdit.charAt(0) != '7' && opcionEdit.charAt(0) != '8');
                 }
                 case '3' -> // regresar al menu principal
                     System.out.println("Volviendo al menú principal.....\n");
                 default -> System.out.println("Opción no válida. Por favor, ingresa un número del 1 al 3.");
             }
             // Código para editar emprendedor
-                    }while(opcion.charAt(0) != '3');
+        }while(opcion.charAt(0) != '3');
     }
 }
